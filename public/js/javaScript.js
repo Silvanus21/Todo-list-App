11722f801a46b2fb4c784d838e0efbc8b7a7a9ec
Silvanus <selvasilvanus@gmail.com>
2020-09-09 16:28:48 +0530

    Improved responsiveness

--- public/js/javaScript.js
+++ public/js/javaScript.js
@@ -1,13 +1,1 @@
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {};
-var $checkboxes = $(".round :checkbox");
-
-$checkboxes.on("change", function(){
-  $checkboxes.each(function(){
-    checkboxValues[this.id] = this.checked;
-  });
-  localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
-});
-
-$.each(checkboxValues, function(key, value) {
-    $("#" + key).prop('checked', value);
-  });
+var checkboxValues=JSON.parse(localStorage.getItem("checkboxValues"))||{},$checkboxes=$(".round :checkbox");$checkboxes.on("change",(function(){$checkboxes.each((function(){checkboxValues[this.id]=this.checked})),localStorage.setItem("checkboxValues",JSON.stringify(checkboxValues))})),$.each(checkboxValues,(function(e,c){$("#"+e).prop("checked",c)}));
